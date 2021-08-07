17b2a03c588cd5a804d9bf22fc11034ade4db347
Abraham2021 <gutierrezabraham89@gmail.com>
2021-08-06 19:03:36 -0600

    Updates

---
+++ precache-manifest.730fb6aa166dfe8430ea0d633be05453.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ce5a143d8b1c054f476c41a43935c9",
+    "url": "/Covid-19-Rest-Apis/index.html"
+  },
+  {
+    "revision": "a1dc99f81cf8543423dd",
+    "url": "/Covid-19-Rest-Apis/static/css/2.15f857eb.chunk.css"
+  },
+  {
+    "revision": "b66a48dc94242e8539ff",
+    "url": "/Covid-19-Rest-Apis/static/css/main.43e82ff5.chunk.css"
+  },
+  {
+    "revision": "a1dc99f81cf8543423dd",
+    "url": "/Covid-19-Rest-Apis/static/js/2.710f64ed.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-19-Rest-Apis/static/js/2.710f64ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66a48dc94242e8539ff",
+    "url": "/Covid-19-Rest-Apis/static/js/main.15e420ee.chunk.js"
+  },
+  {
+    "revision": "0807a3cd4074f481a21d",
+    "url": "/Covid-19-Rest-Apis/static/js/runtime-main.ab54bae0.js"
+  }
+]);
